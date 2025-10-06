4605181824944b282406de0a7a94394070e86148
Sachin Saini <iamsainisachin@gmail.com>
2025-10-07 02:44:23 +0530

    chore: move from functions to abbrivations

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
 # global variables
 set -gx EDITOR nano
 set -gx BUN_INSTALL ~/.bun
+set -gx HISTSIZE 10000
+set -gx SAVEHIST 10000
 
 # path mutations
 fish_add_path /opt/homebrew/bin
@@ -10,6 +12,7 @@ fish_add_path ~/tools/kafka/bin
 fish_add_path $BUN_INSTALL/bin
 
 # abbrs
+# git
 abbr --add cat bat
 abbr --add codex codex --search
 abbr --add grep rg
@@ -23,7 +26,18 @@ abbr --add glg git log --oneline
 abbr --add gf git fetch
 abbr --add gco git checkout
 abbr --add gcl git clone
-abbr --add git checkout -b
+abbr --add gcb git checkout -b
 abbr --add gc git commit -v
 abbr --add gaa git add .
+abbr --add gca git commit --amend
+abbr --add gcane git commit --amend --no-edit
+abbr --add gsh git stash
+abbr --add gshp git stash pop
+abbr --add gb git branch
+
+# fish
+abbr --add reload source ~/.config/fish/config.fish
+
+
+
 
